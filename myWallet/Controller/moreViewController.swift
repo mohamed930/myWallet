ca8acc60bc6170bd5b8f8757c84c413b910885d8
mohamed930 <mohammedali12477@gmail.com>
2023-10-07 00:58:26 +0300

    finish more screen and relate pages with tabBar commit

---
+++ myWallet/Controller/moreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  moreViewController.swift
+//  myWallet
+//
+//  Created by Mohamed Ali on 07/10/2023.
+//
+
+import UIKit
+
+class moreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
